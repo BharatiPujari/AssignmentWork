5a64edce1e0a526cb56a6b61302fe112dbae6c27
BharatiPujari <106653431+BharatiPujari@users.noreply.github.com>
2023-04-19 14:56:29 +0530

    Add files via upload

---
+++ 08_strings/04_str_cmp/04_str_cmp.c
@@ -0,0 +1,39 @@
+/*********************************************************************************************
+ * © 2022 RuggedBOARRD. - All Rights Reserved. Permission to use, modify, copy, and distribute
+ * this source code, object code, or executable code (collectively, Software), is granted only
+ * under the terms of a valid written license agreement with RuggedBOARD. Unauthorized copying
+ * or other use of the Software is strictly prohibited.  Software is owned by and constitutes
+ * the proprietary works, trade secrets, and copyrights of Embitel or its licensors.
+ * For further information, contact community.ruggedboard.com / info@ruggedboard.com
+ *
+ * Application: 
+ * Brief: 
+ * Author: Bharati Kollur ( bharatisk23@gmail.com )
+ * Title: Project Engineer
+ * Last Modified Date: 19.09.2022
+ *
+/*********************************************************************************************/
+
+// Header File
+#include <stdio.h>
+#include<string.h>
+
+// Main Function
+int main()
+{
+   char str[20], password[] = "danger";
+
+   printf("Please enter your password: ");
+   scanf("%[^\n]s", str); // Use %[^\n]s instead of %s to read the entire line
+
+   if(strcmp(str, password) == 0){
+      printf("Entered correct password\n");
+   }
+   else{
+      printf("Wrong password\n");
+   }
+     
+     return 0;
+}
+
+// Program End
